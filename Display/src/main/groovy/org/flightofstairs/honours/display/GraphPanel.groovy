b63e7437815a915ce38dc2c1203ea7fe08333553
Alistair Smith <github@flightofstairs.org>
2012-01-13 19:05:00 +0000

    Animated class relation view.

--- Display/src/main/groovy/org/flightofstairs/honours/display/GraphPanel.groovy
+++ Display/src/main/groovy/org/flightofstairs/honours/display/GraphPanel.groovy
@@ -4,12 +4,19 @@ import java.awt.Dimension;
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
 
+import 
+
 import edu.uci.ics.jung.algorithms.layout.AbstractLayout;
 import edu.uci.ics.jung.algorithms.layout.FRLayout2;
+import edu.uci.ics.jung.algorithms.layout.*;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.renderers.Renderer;
 import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
 import edu.uci.ics.jung.algorithms.layout.util.Relaxer;
+import edu.uci.ics.jung.visualization.layout.LayoutTransition;
+import edu.uci.ics.jung.visualization.util.Animator;
+import edu.uci.ics.jung.algorithms.layout.util.VisRunner;
+import edu.uci.ics.jung.algorithms.util.IterativeContext;
 
 import org.flightofstairs.honours.common.CallGraph;
 import org.flightofstairs.honours.common.CallGraphListener;
@@ -18,8 +25,10 @@ import org.gcontracts.annotations.*
 
 @Invariant({ layout != null })
 public class GraphPanel<V extends Serializable> extends JPanel implements CallGraphListener {
-	public static final int DEFAULT_X = 600;
-	public static final int DEFAULT_Y = 600;	
+	public static final int DEFAULT_X = 800;
+	public static final int DEFAULT_Y = 800;
+	
+	public static final Transformer
 	
 	private final CallGraph<V> callGraph;
 	
@@ -32,24 +41,44 @@ public class GraphPanel<V extends Serializable> extends JPanel implements CallGr
 		
 		layout = new FRLayout2(callGraph.getGraph());
 		
-		viewer = new VisualizationViewer<Number,Number>(layout, new Dimension(DEFAULT_X, DEFAULT_Y));
-		vv.getRenderer().getVertexLabelRenderer().setPosition(Renderer.VertexLabel.Position.CNTR);
-		vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller<V>());
+		layout.setSize(new Dimension(DEFAULT_X, DEFAULT_Y));
 		
-		getContentPane().setLayout(new BorderLayout());
-		getContentPane().add(viewer);
+		Relaxer relaxer = new VisRunner((IterativeContext)layout);
+		relaxer.stop();
+		relaxer.prerelax();
+
+		Layout staticLayout = new StaticLayout(callGraph.getGraph(), layout);
+
+        viewer = new VisualizationViewer(staticLayout, new Dimension(DEFAULT_X, DEFAULT_Y));
+		
+		viewer.getRenderer().getVertexLabelRenderer().setPosition(Renderer.VertexLabel.Position.CNTR);
+		viewer.getRenderContext().setVertexLabelTransformer(new ToStringLabeller<V>());
+		
+		setLayout(new BorderLayout());
+		add(viewer);
 		
 		validate();
-		pack();
+		
+		callGraph.addListener(this);
 	}
 	
 	@Requires({callGraph == this.callGraph})
 	public void callGraphChange(CallGraph callGraph) {
-		Relaxer relaxer = viewer.getModel().getRelaxer();
-        relaxer.pause();
-		
 		layout.initialize();
 		
+		Relaxer relaxer = new VisRunner((IterativeContext)layout);
+        relaxer.stop();
+		
+		relaxer.prerelax();
+        StaticLayout staticLayout = new StaticLayout(callGraph.getGraph(), layout);
+		
+		LayoutTransition lt = new LayoutTransition(viewer, viewer.getGraphLayout(), staticLayout);
+		
+		Animator animator = new Animator(lt);
+		animator.start();
+		
+		viewer.repaint();
+
 		relaxer.resume();
 	}
 }
