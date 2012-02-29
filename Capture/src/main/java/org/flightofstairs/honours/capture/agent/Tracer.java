9938a74b70ab4fcaaee96429975f8b5db4c52d4e
Alistair Smith <alistairsmith@flightofstairs.org>
2012-02-29 12:19:47 +0000

    Huge performance improvements. Memory usage from > 1.5GiB to less than 300MiB. Speed increase of more than 20 times. Introduced limits on number of calls waiting to 50000; some will be dropped, but there's no crashes now.

--- Capture/src/main/java/org/flightofstairs/honours/capture/agent/Tracer.java
+++ Capture/src/main/java/org/flightofstairs/honours/capture/agent/Tracer.java
@@ -7,6 +7,7 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.flightofstairs.honours.capture.recorder.RemoteRecorder;
@@ -15,10 +16,12 @@ import org.flightofstairs.honours.common.Call;
 public enum Tracer {
 	INSTANCE;
 	
-	/**
-	 * Time between trace submits in ms.
-	 */ 
+
 	public static final int SUBMIT_DELAY = 50;
+	
+	public static final int MAX_WAITING = 50000;
+	
+	public final AtomicInteger waiting = new AtomicInteger(0);
 	
 	private final Queue<Call> toSend = new ConcurrentLinkedQueue<Call>();
 	
@@ -44,7 +47,8 @@ public enum Tracer {
 	}
 	
 	public void traceCall(Call call) {
-			toSend.add(call);
+		toSend.add(call);
+		waiting.incrementAndGet();
 	}
 	
 	
@@ -53,15 +57,8 @@ public enum Tracer {
 			@Override
 			public void run() {
 				try {
-					List<Call> toSendCopy = new LinkedList<Call>();
+					submit();
 					
-					Call polled;
-					
-					while((polled = toSend.poll()) != null) {
-						toSendCopy.add(polled);
-					}
-					
-					recorder.addCalls(toSendCopy);
 					recorder.end();
 				} catch (RemoteException ex) {
 				}
@@ -71,27 +68,40 @@ public enum Tracer {
 		Runtime.getRuntime().addShutdownHook(thread);
 	}
 	
+	private void submit() {
+		List<Call> toSendCopy;
+
+		synchronized(toSend) {
+			toSendCopy = new LinkedList<Call>(toSend);
+			toSend.clear();
+			
+			waiting.set(0);
+		}
+		
+		final Map<Call, Integer> callCounts = new HashMap<Call, Integer>();
+
+		for(Call c : toSendCopy) {
+			final int count = callCounts.containsKey(c) ? callCounts.get(c) + 1 : 1;
+			callCounts.put(c, count);
+		}
+
+		try {
+			recorder.addCallCounts(callCounts);
+			toSend.clear();
+		} catch (ConnectException ex) {
+			Logger.getLogger(Tracer.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (RemoteException ex) {
+			Logger.getLogger(Tracer.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+	
 	private void initSubmit() {
 		final Timer timer = new Timer();
 		final TimerTask task = new TimerTask() {
 			
 			@Override
 			public void run() {
-				List<Call> toSendCopy;
-				
-				synchronized(toSend) {
-					toSendCopy = new LinkedList<Call>(toSend);
-					toSend.clear();
-				}
-				
-				try {
-					recorder.addCalls(toSendCopy);
-					toSend.clear();
-				} catch (ConnectException ex) {
-					Logger.getLogger(Tracer.class.getName()).log(Level.SEVERE, null, ex);
-				} catch (RemoteException ex) {
-					Logger.getLogger(Tracer.class.getName()).log(Level.SEVERE, null, ex);
-				}
+				submit();
 			}
 		};
 		timer.schedule(task, SUBMIT_DELAY, SUBMIT_DELAY);
--- Capture/src/main/java/org/flightofstairs/honours/capture/recorder/RemoteRecorder.java
+++ Capture/src/main/java/org/flightofstairs/honours/capture/recorder/RemoteRecorder.java
@@ -3,9 +3,11 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
+import java.util.Map;
 import org.flightofstairs.honours.common.Call;
 
 public interface RemoteRecorder extends Remote {
-	public void addCalls(List<Call> calls) throws RemoteException;
 	public void end() throws RemoteException;
+
+	public void addCallCounts(Map<Call, Integer> callCounts) throws RemoteException;
 }
